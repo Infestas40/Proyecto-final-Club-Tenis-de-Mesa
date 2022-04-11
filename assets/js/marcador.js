c122f4521bb171438c0479bcbbd0b028f5742ca4
Infestas40 <locuras_metallicas@hotmail.com>
2022-04-11 14:58:44 +0200

    subidacasa

--- assets/js/marcador.js
+++ assets/js/marcador.js
@@ -1,1 +1,11 @@
 
+let counter = 0;
+
+function countingClicks(){
+    document.getElementById("counting").innerHTML = ++counter;
+    if (counter > 10){
+        document.getElementById("warning").innerHTML = message;
+    }
+}
+
+
