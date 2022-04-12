59f9b98d3824e0cc1ca86ebfe63edce0307f73b4
unknown <Infestas36>
2022-04-12 14:45:28 +0200

    proyecto

--- assets/js/marcador.js
+++ assets/js/marcador.js
@@ -1,11 +1,36 @@
 
 let counter = 0;
 
+
 function countingClicks(){
     document.getElementById("counting").innerHTML = ++counter;
     if (counter > 10){
-        document.getElementById("warning").innerHTML = message;
     }
 }
+
+function restarClicks(){
+    document.getElementById("counting").innerHTML = --counter;
+    if (counter > 10){
+        
+    }
+}
+
+function countingClicksmarcadorplayer2(){
+    document.getElementById("counting2").innerHTML = ++counter;
+    if (counter > 10){
+        
+    }
+}
+
+function restarClicksmarcadorplayer2(){
+    document.getElementById("counting2").innerHTML = --counter;
+    if (counter > 10){
+        
+    }
+}
+
+
+
+
 
 
